26770fe6e9043480bbc7a00615de2dc1fd3836a9
serj <misterds1@mail.ru>
2017-08-10 16:26:40 -0400

    add modification in the  code.js

---
+++ code.js
@@ -0,0 +1,16 @@
+$(function(/*
+
+	FileReader
+
+	readyState
+
+	Return Type:
+	unsigned short
+
+	Description:
+	The FileReader object can be in one of 3 states.
+
+	URL doc:
+	http://html5index.org/File%20API%20-%20FileReader.html#readyState
+
+*/))
